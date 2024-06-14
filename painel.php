d15930265b0b849f320889f9dad07be7c4b2dc49
Davi Silva <davi-fernandosilva@hotmail.com>
2024-06-13 22:29:40 -0300

    Atualiza arquivo de painel para visualização

--- painel.php
+++ painel.php
@@ -7,44 +7,39 @@
     <link rel="stylesheet" href="css/painel.css">
     <title>Painel</title>
 </head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Painel de Usuarios</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-        <div class="button-sair">
-            <a href="index.php" class="btn btn-danger me-5">Sair</a> <!-- comando sair para retornar a pagima de login do usuario no sistema -->
-        </div>
-    </nav>
-    <br>
-    <?php
-        //echo "<h1>Bem vindo <u>Usuario</u></h1>"; tag de bem vindo ao usuario ou admin no sistema
-    ?>
-    <table class="table">
-     <thead>
-     <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nome</th>
-      <th scope="col">E-mail</th>
-      <th scope="col">Celular</th>
-      <th scope="col">Assunto</th>
-      <th scope="col">Arquivo</th>
-      
-    </tr>
-    </thead>
-    <tbody>
-     <?php
-        //while($user_data = mysql_fetch_assoc($resultado)) codigo de preenchimento automatico do painel de usuarios, apenas trocar as variaveis...
-        //{
-            //echo "<tr>"; 
-            //echo "<td>".$user_data['id']. "<td>";
-        //}
-      ?>
-    </tbody>
-    </table>
-    </div>
+<?php
+
+include("crud/conexao.php"); ?>
+
+   <?php
+       $stmt = $conn->prepare("select * from contato");
+       $stmt->execute(); 
+   ?>
+   
+   <table class="table table-striped">
+       <thead>
+           <tr>
+           <th scope="col">#</th>
+               <th scope="col">Nome</th>
+               <th scope="col">E-mail</th>
+               <th scope="col">Celular</th>
+               <th scope="col">Assunto</th>
+               <th scope="col">Arquivo</th>
+           </tr>
+     </thead>
+     <tbody>    
+       <?php
+           while($row = $stmt->fetch()) {
+               echo "<tr>";
+               echo "<td> $row[idContato] </td>";                             
+                   echo "<td> $row[contatoNome] </td>";
+                   echo "<td> $row[contatoEmail] </td>";
+                   echo "<td> $row[contatoNumero] </td>";
+                   echo "<td> $row[contatoAssunto] </td>";
+                   echo "<td> $row[contatoFile] </td>";
+               echo "</tr>";
+           }
+       ?>
+     </tbody>
+   </table>
 </body>
-</html>
